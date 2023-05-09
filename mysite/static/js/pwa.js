30181a5a783cffd79414fb342b15ec9bfe32fb1d
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-09 22:42:17 +0000

    atual

--- mysite/static/js/pwa.js
+++ mysite/static/js/pwa.js
@@ -1,6 +1,6 @@
 function registerServiceWorker() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('service-worker.js')
+      navigator.serviceWorker.register('/')
         .then(registration => {
           console.log('Service Worker registrado com sucesso:', registration.scope);
         })
