fcb7cd16ea666b2e562b04a056333505539a16e6
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-10 18:46:53 +0000

    salvo

--- mysite/static/js/service-worker.js
+++ mysite/static/js/service-worker.js
@@ -2,26 +2,11 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const CACHE_NAME = 'conversao-cache-v1';
 
-const offline = "index.html";
-
 const urlsToCache = [
   '/',
   'index.html',
   '/static/css/style.css'
 ];
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.add(offline))
-  );
-});
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -30,37 +15,11 @@ self.addEventListener('install', event => {
   );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
 });
 
 self.addEventListener('activate', event => {
