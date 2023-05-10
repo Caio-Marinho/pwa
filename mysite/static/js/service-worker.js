06129a9d07eb6dda85674271f21f68bbc976c142
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-10 20:31:15 +0000

    a

--- mysite/static/js/service-worker.js
+++ mysite/static/js/service-worker.js
@@ -5,7 +5,6 @@ const CACHE_NAME = 'conversao-cache-v1';
 const urlsToCache = [
   '/',
   'index.html',
-  '/conversao',
   '/static/css/style.css',
   '/static/bootstrap/css/bootstrap-grid.min.css',
   '/static/bootstrap/js/bootstrap.bundle.min.js',
