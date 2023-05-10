0d8b7d30782e5ef06c16f2ed7a81c671f5e1bb62
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-10 11:41:09 +0000

    ok

--- mysite/static/js/service-worker.js
+++ mysite/static/js/service-worker.js
@@ -26,7 +26,7 @@ self.addEventListener('activate', event => {
       .then(cacheNames => {
         return Promise.all(
           cacheNames.filter(cacheName => {
-            return cacheName.startsWith('Conversão-cache-v1') &&
+            return cacheName.startsWith('Conversão-cache-') &&
                    cacheName !== CACHE_NAME;
           }).map(cacheName => {
             return caches.delete(cacheName);
