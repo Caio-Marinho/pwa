fa5d0ce53d0ef3fb7a8555512a76ec1a91a3636c
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-11 11:37:08 +0000

    atual

--- mysite/static/js/exibir.js
+++ mysite/static/js/exibir.js
@@ -1,10 +1,19 @@
 function exibir() {
-        fetch('/conversao') // faz uma requisição GET para o servidor
-          .then(response => response.json()) // converte a resposta para JSON
-          .then(data => { // atualiza o valor na página
-            var conversao = data.conversao;
-            document.getElementById('conversao').innerHTML = conversao;
-          })
-          .catch(error => console.error(error));
+  $(document).ready(function() {
+    $('#btn-consultar').click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: '/conversao',
+            type: 'POST',
+            data: $('#form-conversao').serialize(),
+            success: function(response) {
+                $('#conversao').html(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
+});
       }
 exibir(); // chama a função ao carregar a página
