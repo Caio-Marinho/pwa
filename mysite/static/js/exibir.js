35fd9997da3c1d9a4c1fa2217ee8dd676ca4e300
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-11 19:04:33 +0000

    a

--- mysite/static/js/exibir.js
+++ mysite/static/js/exibir.js
@@ -1,29 +1,22 @@
 function realizarConversao() {
-  $('#form-conversao').submit(function(event) {
+  fetch('/conversao_valor')
+    .then(response => response.json())
+    .then(data => {
+      const valor = data.valor;
+      // Faça algo com o valor retornado
+    })
+    .catch(error => console.error(error));
+}
+
+$('#form-conversao').submit(function(event) {
   event.preventDefault();
   $.ajax({
-  url: '/conversao',
-  type: 'POST',
-  data: $('#form-conversao').serialize(),
-  success: function(response) {
-  $('#conversao').text(response.valor);
-  // Faz outra requisição AJAX para atualizar o valor armazenado em cache
-  atualizarValorEmCache();
-  }
+    url: '/conversao',
+    type: 'POST',
+    data: $('#form-conversao').serialize(),
+    success: function(response) {
+      $('#conversao').text(response.valor);
+      atualizarConteudo();
+    }
   });
-  });
-  }
-  
-  function atualizarValorEmCache() {
-  $.ajax({
-  url: '/conversao_valor',
-  type: 'GET',
-  success: function(response) {
-  console.log(response.valor);
-  }
-  });
-  }
-  
-  $(document).ready(function() {
-  realizarConversao();
-  });
+});
