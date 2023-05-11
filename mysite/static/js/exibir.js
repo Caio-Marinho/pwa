984d559ae30c8a5658a1985d02715a8d9cccc4dc
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-11 18:44:34 +0000

    a

--- mysite/static/js/exibir.js
+++ mysite/static/js/exibir.js
@@ -1,14 +1,21 @@
-$('#conversao-form').submit(function(e) {
-  e.preventDefault();
-  $.ajax({
-    type: 'POST',
-    url: '/conversao',
-    data: $('#conversao-form').serialize(),
-    success: function(response) {
-      $('#resultado').text(response.valor);
-    },
-    error: function() {
-      $('#resultado').text('Erro ao converter valor');
-    }
+$(document).ready(function() {
+  $('#form-conversao').submit(function(event) {
+      event.preventDefault();
+      $.ajax({
+          url: '/conversao',
+          type: 'POST',
+          data: $('#form-conversao').serialize(),
+          success: function(response) {
+              $('#conversao').text(response.valor);
+              // Faz outra requisição AJAX para atualizar o valor armazenado em cache
+              $.ajax({
+                  url: '/conversao_valor',
+                  type: 'GET',
+                  success: function(response) {
+                      console.log(response.valor);
+                  }
+              });
+          }
+      });
   });
 });
