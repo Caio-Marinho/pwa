a04b1537f080e7b6bc81ca0616f4d887c69e836e
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-11 11:56:50 +0000

    a

--- mysite/static/js/exibir.js
+++ mysite/static/js/exibir.js
@@ -1,20 +1,16 @@
-function exibir() {
-  $(document).ready(function() {
-    $('#btn-consultar').click(function(event) {
-        event.preventDefault();
-        $.ajax({
-            url: '/conversao',
-            type: 'POST',
-            data: $('#form-conversao').serialize(),
-            success: function(response) {
-                console.log(response);
-                $('#conversao').html(response);
-            },
-            error: function(error) {
-                console.log(error);
-            }
-        });
-    });
+  // Adiciona um listener para o submit do form
+  document.getElementById("form-conversao").addEventListener("submit", function(event) {
+    event.preventDefault(); // Previne o comportamento padrão do form (submit e refresh da página)
+    var form = new FormData(event.target); // Obtém os dados do form
+    var xhr = new XMLHttpRequest(); // Cria uma nova requisição AJAX
+    xhr.open('POST', '/conversao'); // Configura a requisição para enviar os dados do form para o endpoint '/conversao'
+    xhr.onload = function() {
+        if (xhr.status === 200) { // Verifica se a requisição foi bem sucedida
+            var response = JSON.parse(xhr.responseText); // Converte a resposta para JSON
+            document.getElementById("resultado").textContent = response.valor; // Atualiza o elemento HTML com o valor convertido
+        } else {
+            console.log('Erro ao realizar a requisição.');
+        }
+    };
+    xhr.send(form); // Envia a requisição com os dados do form
 });
-      }
-exibir(); // chama a função ao carregar a página
