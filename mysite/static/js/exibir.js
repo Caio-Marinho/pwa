2d65fb153d60f289e111a681da7951195c92feef
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-11 19:13:26 +0000

    a

--- mysite/static/js/exibir.js
+++ mysite/static/js/exibir.js
@@ -1,22 +1,25 @@
-function realizarConversao() {
+function atualizarConteudo() {
   fetch('/conversao_valor')
     .then(response => response.json())
     .then(data => {
       const valor = data.valor;
-      // Faça algo com o valor retornado
+      console.log(valor);
     })
     .catch(error => console.error(error));
 }
 
-$('#form-conversao').submit(function(event) {
+document.querySelector('#form-conversao').addEventListener('submit', function(event) {
   event.preventDefault();
-  $.ajax({
-    url: '/conversao',
-    type: 'POST',
-    data: $('#form-conversao').serialize(),
-    success: function(response) {
-      $('#conversao').text(response.valor);
-      realizarConversao();
-    }
-  });
+  const form = new FormData(this);
+  const options = {
+    method: 'POST',
+    body: form
+  };
+  fetch('/conversao', options)
+    .then(response => response.json())
+    .then(data => {
+      document.querySelector('#conversao').textContent = data.valor;
+      atualizarConteudo();
+    })
+    .catch(error => console.error(error));
 });
