1fbc5340249ac938aea055ab407c0e7314d32c18
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-11 18:58:17 +0000

    a

--- mysite/static/js/atualizar.js
+++ mysite/static/js/atualizar.js
@@ -16,3 +16,32 @@ function atualizarConteudo() {
 }
 
 atualizarConteudo();
+function realizarConversao() {
+    $('#form-conversao').submit(function(event) {
+    event.preventDefault();
+    $.ajax({
+    url: '/conversao',
+    type: 'POST',
+    data: $('#form-conversao').serialize(),
+    success: function(response) {
+    $('#conversao').text(response.valor);
+    // Faz outra requisição AJAX para atualizar o valor armazenado em cache
+    atualizarValorEmCache();
+    }
+    });
+    });
+    }
+    
+    function atualizarValorEmCache() {
+    $.ajax({
+    url: '/conversao_valor',
+    type: 'GET',
+    success: function(response) {
+    console.log(response.valor);
+    }
+    });
+    }
+    
+    $(document).ready(function() {
+    realizarConversao();
+    });
