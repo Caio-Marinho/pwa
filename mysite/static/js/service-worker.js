435483a47f76108dbf86a9af65b3c332e09defed
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-12 11:42:07 +0000

    a

--- mysite/static/js/service-worker.js
+++ mysite/static/js/service-worker.js
@@ -1,40 +1,18 @@
+// This is the "Offline copy of pages" service worker
 
-const CACHE_NAME = 'conversao-cache-v1';
+const CACHE = "pwabuilder-offline";
 
-const urlsToCache = [
-  '/',
-  '/templates/index.html',
-  '/static/css/style.css',
-  '/static/bootstrap/css/bootstrap-grid.min.css',
-  '/static/bootstrap/js/bootstrap.bundle.min.js',
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.filter(cacheName => {
-            return cacheName.startsWith('conversao-cache-') &&
-                   cacheName !== CACHE_NAME;
-          }).map(cacheName => {
-            return caches.delete(cacheName);
-          })
-        );
-      })
-  );
-});
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
