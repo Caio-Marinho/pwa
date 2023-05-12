eb30af039d06cfba57513bc00a46abc3f3b858c1
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-12 12:30:23 +0000

    axios

--- mysite/static/js/atualizar.js
+++ mysite/static/js/atualizar.js
@@ -18,41 +18,32 @@ function atualizarConteudo() {
 atualizarConteudo();
 
 function realizarConversao() {
-    var form = document.querySelector('#form-conversao');
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', '/conversao');
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          var response = JSON.parse(xhr.responseText);
-          document.getElementById('conversao').textContent = response.valor;
-          // Faz outra requisição AJAX para atualizar o valor armazenado em cache
-          atualizarValorEmCache();
-        } else {
-          console.log('Erro na requisição. Status do erro: ' + xhr.status);
-        }
-      };
-      xhr.send(new FormData(form));
-    });
-  }
-  
-  function atualizarValorEmCache() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/conversao_valor');
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        console.log(response.valor);
-      } else {
-        console.log('Erro na requisição. Status do erro: ' + xhr.status);
-      }
-    };
-    xhr.send();
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    realizarConversao();
+  var form = document.querySelector('#form-conversao');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    var data = new FormData(form);
+    axios.post('/conversao', data)
+      .then(function(response) {
+        document.querySelector('#conversao').textContent = response.data.valor;
+        // Faz outra requisição AJAX para atualizar o valor armazenado em cache
+        atualizarValorEmCache();
+      })
+      .catch(function(error) {
+        console.log('Erro na requisição. Status do erro: ' + error.response.status);
+      });
   });
-  
+}
+
+function atualizarValorEmCache() {
+  axios.get('/conversao_valor')
+    .then(function(response) {
+      console.log(response.data.valor);
+    })
+    .catch(function(error) {
+      console.log('Erro na requisição. Status do erro: ' + error.response.status);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  realizarConversao();
+});
