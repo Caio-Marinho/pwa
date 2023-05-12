359e5dbaf67e8a9592f59f32e62662ec469b244a
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-12 13:13:18 +0000

    a

--- mysite/static/js/atualizar.js
+++ mysite/static/js/atualizar.js
@@ -19,33 +19,13 @@ atualizarConteudo();
 
 function realizarConversao() {
   var form = document.querySelector('#form-conversao');
-  var btnConverter = document.querySelector('#btn-converter');
-  btnConverter.addEventListener('click', function(event) {
-    event.preventDefault();
-    var data = new FormData(form);
-    axios.post('/conversao', data)
-      .then(function(response) {
-        document.querySelector('#conversao').textContent = response.data.valor;
-        // Faz outra requisição AJAX para atualizar o valor armazenado em cache
-        atualizarValorEmCache();
-      })
-      .catch(function(error) {
-        console.log('Erro na requisição. Status do erro: ' + error.response.status);
-      });
-  });
-}
-
-function atualizarValorEmCache() {
-  axios.get('/conversao_valor')
+  var data = new FormData(form);
+  axios.post('/conversao', data)
     .then(function(response) {
-      console.log(response.data.valor);
+      document.querySelector('#conversao').textContent = response.data.valor;
     })
     .catch(function(error) {
       console.log('Erro na requisição. Status do erro: ' + error.response.status);
     });
+  return false;
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  realizarConversao();
-});
-
