bfc0b95bdc92471f45213defbbd5a4152611e8a7
kaigabriel12 <108732958+kaigabriel12@users.noreply.github.com>
2023-05-12 11:25:51 +0000

    atual

--- mysite/static/js/service-worker.js
+++ mysite/static/js/service-worker.js
@@ -7,6 +7,7 @@ const urlsToCache = [
   '/static/css/style.css',
   '/static/bootstrap/css/bootstrap-grid.min.css',
   '/static/bootstrap/js/bootstrap.bundle.min.js',
+  'atualiza.js',
 ];
 
 self.addEventListener('install', event => {
